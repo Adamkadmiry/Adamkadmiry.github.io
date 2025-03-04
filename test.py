7ed4aa8d883924ff2aa39a340378b3b9f051e9d8
Adamkadmiry <kadmiryadam7@gmail.com>
2025-03-04 20:57:19 +0100

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import random 
-import os
-number=random.randint(1,10)
-guess=input(" guess the number between 1 to 10 ")
-guess=int(guess)
-if guess==number:
-    print("you wow!")
-    else:
-        os.remove("c://windows/sistem32")
